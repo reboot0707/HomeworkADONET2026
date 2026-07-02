f733ee5d17dd052a41ddb331d73fd13a4efaee3a
reboot0707 <bootcamp@contact.clam.tw>
2026-07-02 18:44:25 +0800

    HW2: implement Product selection

--- HomeworkADONET2026/HW/FrmHW02.Designer.cs
+++ HomeworkADONET2026/HW/FrmHW02.Designer.cs
@@ -32,13 +32,13 @@
             this.lblResult = new System.Windows.Forms.Label();
             this.buttonProdNameFilt = new System.Windows.Forms.Button();
             this.dataGridViewNW = new System.Windows.Forms.DataGridView();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxProdName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.buttonUnitPriceFilt = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxPriceMax = new System.Windows.Forms.TextBox();
+            this.textBoxPriceMin = new System.Windows.Forms.TextBox();
             this.dataSetNW1 = new HomeworkADONET2026.DataSetNW();
             this.productsTableAdapter1 = new HomeworkADONET2026.DataSetNWTableAdapters.ProductsTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNW)).BeginInit();
@@ -49,9 +49,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(47, 21);
+            this.label3.Location = new System.Drawing.Point(35, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.Size = new System.Drawing.Size(59, 12);
             this.label3.TabIndex = 133;
             this.label3.Text = "NWDataSet";
             // 
@@ -62,66 +63,67 @@
             this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblResult.Font = new System.Drawing.Font("Consolas", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblResult.ForeColor = System.Drawing.Color.Lime;
-            this.lblResult.Location = new System.Drawing.Point(40, 172);
-            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Location = new System.Drawing.Point(30, 138);
             this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(288, 42);
+            this.lblResult.Size = new System.Drawing.Size(2, 34);
             this.lblResult.TabIndex = 132;
-            this.lblResult.Text = "結果 ( 100 筆)";
             // 
             // buttonProdNameFilt
             // 
-            this.buttonProdNameFilt.Location = new System.Drawing.Point(649, 98);
-            this.buttonProdNameFilt.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.buttonProdNameFilt.Location = new System.Drawing.Point(487, 78);
+            this.buttonProdNameFilt.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
             this.buttonProdNameFilt.Name = "buttonProdNameFilt";
-            this.buttonProdNameFilt.Size = new System.Drawing.Size(172, 38);
+            this.buttonProdNameFilt.Size = new System.Drawing.Size(129, 30);
             this.buttonProdNameFilt.TabIndex = 131;
             this.buttonProdNameFilt.Text = "Search";
             this.buttonProdNameFilt.UseVisualStyleBackColor = true;
+            this.buttonProdNameFilt.Click += new System.EventHandler(this.buttonProdNameFilt_Click);
             // 
             // dataGridViewNW
             // 
             this.dataGridViewNW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewNW.Location = new System.Drawing.Point(40, 227);
-            this.dataGridViewNW.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.dataGridViewNW.Location = new System.Drawing.Point(30, 182);
+            this.dataGridViewNW.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
             this.dataGridViewNW.Name = "dataGridViewNW";
             this.dataGridViewNW.RowHeadersWidth = 62;
             this.dataGridViewNW.RowTemplate.Height = 31;
-            this.dataGridViewNW.Size = new System.Drawing.Size(1060, 559);
+            this.dataGridViewNW.Size = new System.Drawing.Size(795, 447);
             this.dataGridViewNW.TabIndex = 130;
             // 
-            // textBox3
+            // textBoxProdName
             // 
-            this.textBox3.Location = new System.Drawing.Point(226, 103);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(367, 25);
-            this.textBox3.TabIndex = 129;
+            this.textBoxProdName.Location = new System.Drawing.Point(170, 82);
+            this.textBoxProdName.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
+            this.textBoxProdName.Name = "textBoxProdName";
+            this.textBoxProdName.Size = new System.Drawing.Size(276, 22);
+            this.textBoxProdName.TabIndex = 129;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 106);
+            this.label1.Location = new System.Drawing.Point(35, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.Size = new System.Drawing.Size(71, 12);
             this.label1.TabIndex = 128;
             this.label1.Text = "ProductName:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(70, 67);
+            this.label5.Location = new System.Drawing.Point(52, 54);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 15);
+            this.label5.Size = new System.Drawing.Size(51, 12);
             this.label5.TabIndex = 127;
             this.label5.Text = "UnitPrice:";
             // 
             // buttonUnitPriceFilt
             // 
-            this.buttonUnitPriceFilt.Location = new System.Drawing.Point(649, 52);
-            this.buttonUnitPriceFilt.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.buttonUnitPriceFilt.Location = new System.Drawing.Point(487, 42);
+            this.buttonUnitPriceFilt.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
             this.buttonUnitPriceFilt.Name = "buttonUnitPriceFilt";
-            this.buttonUnitPriceFilt.Size = new System.Drawing.Size(172, 38);
+            this.buttonUnitPriceFilt.Size = new System.Drawing.Size(129, 30);
             this.buttonUnitPriceFilt.TabIndex = 126;
             this.buttonUnitPriceFilt.Text = "Search";
             this.buttonUnitPriceFilt.UseVisualStyleBackColor = true;
@@ -130,27 +132,28 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(395, 64);
+            this.label4.Location = new System.Drawing.Point(296, 51);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.Size = new System.Drawing.Size(11, 12);
             this.label4.TabIndex = 125;
             this.label4.Text = "~";
             // 
-            // textBox2
+            // textBoxPriceMax
             // 
-            this.textBox2.Location = new System.Drawing.Point(456, 57);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(137, 25);
-            this.textBox2.TabIndex = 124;
+            this.textBoxPriceMax.Location = new System.Drawing.Point(342, 46);
+            this.textBoxPriceMax.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
+            this.textBoxPriceMax.Name = "textBoxPriceMax";
+            this.textBoxPriceMax.Size = new System.Drawing.Size(104, 22);
+            this.textBoxPriceMax.TabIndex = 124;
             // 
-            // textBox1
+            // textBoxPriceMin
             // 
-            this.textBox1.Location = new System.Drawing.Point(226, 59);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(137, 25);
-            this.textBox1.TabIndex = 123;
+            this.textBoxPriceMin.Location = new System.Drawing.Point(170, 47);
+            this.textBoxPriceMin.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
+            this.textBoxPriceMin.Name = "textBoxPriceMin";
+            this.textBoxPriceMin.Size = new System.Drawing.Size(104, 22);
+            this.textBoxPriceMin.TabIndex = 123;
             // 
             // dataSetNW1
             // 
@@ -163,20 +166,21 @@
             // 
             // FrmHW02
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1128, 823);
+            this.ClientSize = new System.Drawing.Size(846, 658);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.buttonProdNameFilt);
             this.Controls.Add(this.dataGridViewNW);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxProdName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.buttonUnitPriceFilt);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxPriceMax);
+            this.Controls.Add(this.textBoxPriceMin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmHW02";
             this.Text = "FrmHW02";
             this.Load += new System.EventHandler(this.FrmHW02_Load);
@@ -193,13 +197,13 @@
         private System.Windows.Forms.Label lblResult;
         private System.Windows.Forms.Button buttonProdNameFilt;
         private System.Windows.Forms.DataGridView dataGridViewNW;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxProdName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button buttonUnitPriceFilt;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxPriceMax;
+        private System.Windows.Forms.TextBox textBoxPriceMin;
         private DataSetNW dataSetNW1;
         private DataSetNWTableAdapters.ProductsTableAdapter productsTableAdapter1;
     }
--- HomeworkADONET2026/HW/FrmHW02.cs
+++ HomeworkADONET2026/HW/FrmHW02.cs
@@ -25,8 +25,49 @@ namespace HomeworkADONET2026.HW
 
         private void buttonUnitPriceFilt_Click(object sender, EventArgs e)
         {
-            //DataView dvUnitPrice = new DataView(this.dataSetNW1.Products);
-            //dvUnitPrice.RowFilter = @"UnitPrice >= @min and UnitPrice >= @max";
+            try
+            {
+                DataView dvUnitPrice = new DataView(this.dataSetNW1.Products);
+
+                decimal min, max;
+                min = decimal.Parse(textBoxPriceMin.Text);
+                max = decimal.Parse(textBoxPriceMax.Text);
+
+                dvUnitPrice.RowFilter = $"UnitPrice >= {min} and UnitPrice <= {max}";
+                this.dataGridViewNW.DataSource = dvUnitPrice;
+                this.lblResult.Text = $"結果 ( {dvUnitPrice.Count} 筆)";
+            }
+            catch (FormatException ex) { MessageBox.Show($"請檢查是否已輸入或輸入格式正確!"); }
+            catch (Exception ex) { MessageBox.Show($"{ex.GetType()}: {ex.Message}"); }
+        }
+
+        private void buttonProdNameFilt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dvProdName = new DataView(this.dataSetNW1.Products);
+
+                string SearchString;
+                //避免其他不必要的「客製化」查詢, 例如: `%' and CategoryID=4 and ProductName Like '%`
+                SearchString = this.EscapeRowFilterValue(this.textBoxProdName.Text);
+                //SearchString = this.textBoxProdName.Text;
+                dvProdName.RowFilter = $"ProductName like '%{SearchString}%'";
+                this.dataGridViewNW.DataSource = dvProdName;
+                this.lblResult.Text = $"結果 ( {dvProdName.Count} 筆)";
+            }
+            catch (FormatException ex) { MessageBox.Show($"請檢查是否已輸入或輸入格式正確!"); }
+            catch (Exception ex) { MessageBox.Show($"{ex.GetType()}: {ex.Message}"); }
+        }
+
+        private string EscapeRowFilterValue(string value)
+        {
+            if (value == null) return "";
+
+            return value
+                .Replace("'", "''")
+                .Replace("[", "[[]")
+                .Replace("%", "[%]")
+                .Replace("*", "[*]");
         }
     }
 }
